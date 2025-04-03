8d387f25d7541ae854b656594f7acd46c110d7bb
shivam92 <shivampurwar92@gmail.com>
2025-04-03 13:18:30 +0530

    poc

---
+++ Accounts/src/main/java/com/smartmind/ms/account/AccountsApplication.java
@@ -0,0 +1,13 @@
+package com.smartmind.ms.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
